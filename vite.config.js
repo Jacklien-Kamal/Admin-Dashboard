4d52da00d58f4a46807b351e4fdec2504ccbd31e
jacklien <159618517+Jacklien-Kamal@users.noreply.github.com>
2025-06-01 00:32:47 +0300

    udo changing vit + index

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   root: '.',
-  build: {
-    rollupOptions: {
-      input: '/src/main.jsx', // or main.tsx
-    },}
 })
