02b0d56202b5dd26612e3bd3be5e8907b5d784fd
jacklien <159618517+Jacklien-Kamal@users.noreply.github.com>
2025-06-01 00:12:50 +0300

    handle delete use & add percentage attend

--- src/pages/users/users.jsx
+++ src/pages/users/users.jsx
@@ -3,13 +3,15 @@ import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import Loader from "../../components/loader/loader";
 import { LuCircleX } from "react-icons/lu";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { MdOutlineModeEdit } from "react-icons/md";
 
 const UserList = () => {
   const { t } = useTranslation();
   const [searchQuery, setSearchQuery] = useState("");
   const [sortKey, setSortKey] = useState("");
   const [sortOrder, setSortOrder] = useState("asc");
-  const [ loading , setLoading ] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const [users, setUsers] = useState([]);
 
@@ -49,8 +51,8 @@ const UserList = () => {
     setIsModalOpen(false);
   };
 
-  const handleDeleteUser = () => {
-    setUsers(users.filter((u) => u.id !== selectedUser.id));
+  const handleDeleteUser = (id) => {
+    setUsers(users.filter((u) => u.id !== id));
     setIsModalOpen(false);
   };
 
@@ -106,154 +108,192 @@ const UserList = () => {
           </Link>
         </div>
       </div>
-{loading?<Loader/>:<>
-      <div className="hidden  border  rounded-xl shadow lg:block overflow-auto">
-        <table className="min-w-full bg-white dark:bg-gray-600    ">
-          <thead className="bg-primary-light dark:bg-primary-dark text-white">
-            <tr>
-              <th
-                className="border p-3 cursor-pointer"
-                onClick={() => toggleSort("id")}
-              >
-                ID
-              </th>
-              <th
-                className="border p-3 cursor-pointer"
-                onClick={() => toggleSort("name")}
-              >
-                User
-              </th>
-              <th className="border p-3">Phone</th>
-              <th
-                className="border p-3 cursor-pointer"
-                onClick={() => toggleSort("email")}
-              >
-                Email
-              </th>
-              <th
-                className="border p-3 cursor-pointer"
-                onClick={() => toggleSort("status")}
-              >
-                Status
-              </th>
-              <th className="border p-3">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <div className="hidden  border  rounded-xl shadow lg:block overflow-auto">
+            <table className="min-w-full table-fixed bg-white dark:bg-gray-600">
+              <thead className="bg-primary-light dark:bg-primary-dark text-white">
+                <tr>
+                  <th
+                    className="px-3 py-1  text-center w-12 cursor-pointer"
+                    onClick={() => toggleSort("id")}
+                  >
+                    ID
+                  </th>
+                  <th
+                    className="px-3 py-2 text-left w-64 cursor-pointer"
+                    onClick={() => toggleSort("name")}
+                  >
+                    User
+                  </th>
+                  <th className="px-3 py-1  text-center w-64">Phone</th>
+                  <th
+                    className="px-3 py-1  text-center w-64 cursor-pointer"
+                    onClick={() => toggleSort("email")}
+                  >
+                    Email
+                  </th>
+                  <th
+                    className="px-3 py-1  text-center w-32 cursor-pointer"
+                    onClick={() => toggleSort("status")}
+                  >
+                    Status
+                  </th>
+                  <th className="px-3 py-1  text-center w-64">
+                    Meetings Attended
+                  </th>
+                  <th className=" px-3 py-1    text-center w-40">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredUsers.map((user) => (
+                  <tr key={user.id} className="hover:bg-gray-100 transition">
+                    <td className="p-2 border text-center">{user.id}</td>
+                    <td className="p-2 border flex items-center gap-3">
+                      <img
+                        src={user.profileImg}
+                        alt={user.name}
+                        className="w-10 h-10 rounded-full"
+                      />
+                      <Link
+                        to={`/users/${user.id}`}
+                        className="text-primary-light dark:text-white hover:underline"
+                      >
+                        {user.name}
+                      </Link>
+                    </td>
+                    <td className="p-2 border text-center">
+                      {user.phoneNumber}
+                    </td>
+                    <td className="p-2 border text-center">{user.email}</td>
+                    <td className=" relative p-2 border text-center">
+                      <span
+                        className={`px-3 py-1 rounded-full text-xs font-medium ${
+                          user.status === "active"
+                            ? "bg-green-100 text-green-800"
+                            : "bg-red-100 text-red-700"
+                        }`}
+                      >
+                        {user.status}
+                      </span>
+                      <span
+                        className={`absolute bottom-7 left-10 w-2 h-2 rounded-full${
+                          user.status === "active"
+                            ?  " bg-green-600"
+                            : " bg-red-700"
+                        } ring-2 ring-white  animate-pulse`}
+                      ></span>
+                    </td>
+                    <td className="p-2 border text-center">
+                      <div className="flex flex-col items-center gap-1">
+                        <span className="text-xs font-medium text-gray-700 dark:text-white">
+                          {user.meetings.filter((m) => m.attended).length} /{" "}
+                          {user.meetings.length} (
+                          {Math.round(
+                            (user.meetings.filter((m) => m.attended).length /
+                              user.meetings.length) *
+                              100
+                          )}
+                          %)
+                        </span>
+                        <progress
+                          className="w-full progress-bar"
+                          value={user.meetings.filter((m) => m.attended).length}
+                          max={user.meetings.length}
+                        />
+                      </div>
+                    </td>
+                    <td className="p-2 border text-center space-x-2">
+                      <button
+                        className="bg-indigo-100 hover:bg-indigo-300 text-indigo-600 px-2 py-1 rounded shadow"
+                        onClick={() => handleEdit(user)}
+                      >
+                        <MdOutlineModeEdit />{" "}
+                      </button>
+                      <button
+                        className="bg-red-200 hover:bg-red-300 text-red-600 px-2 py-1 rounded shadow"
+                        onClick={() => {
+                          setSelectedUser(user);
+                          handleDeleteUser(user.id);
+                        }}
+                      >
+                        <RiDeleteBin6Line />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+                {filteredUsers.length === 0 && (
+                  <tr>
+                    <td colSpan={7} className="text-center p-4 text-gray-500">
+                      No users found.
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+          <div className="block md:hidden space-y-4">
             {filteredUsers.map((user) => (
-              <tr key={user.id} className="hover:bg-gray-100 transition">
-                <td className="border p-2 text-center">{user.id}</td>
-                <td className="border p-2 flex items-center gap-3">
+              <div key={user.id} className="bg-white p-4 rounded shadow">
+                <div className="flex items-center gap-4 mb-2">
                   <img
                     src={user.profileImg}
                     alt={user.name}
-                    className="w-10 h-10 rounded-full"
+                    className="w-16 h-16 rounded-full object-contain"
                   />
-                  <Link
-                    to={`/users/${user.id}`}
-                    className="text-blue-600 hover:underline"
-                  >
-                    {user.name}
-                  </Link>
-                </td>
-                <td className="border p-2 text-center">{user.phoneNumber}</td>
-                <td className="border p-2 text-center">{user.email}</td>
-                <td className="border p-2 text-center">
+                  <div>
+                    <h2 className="font-bold">{user.name}</h2>
+                    <p className="text-sm text-gray-600">{user.email}</p>
+                  </div>
+                </div>
+                <p>
+                  <strong>Phone:</strong> {user.phoneNumber}
+                </p>
+
+                <p>
+                  <strong>Status:</strong>
                   <span
-                    className={`px-2 py-1 rounded-full text-xs ${
+                    className={`ml-1 px-2 py-1 rounded-full text-xs ${
                       user.status === "active"
-                        ? "bg-green-400 text-green-700"
+                        ? "bg-green-100 text-green-700"
                         : "bg-red-100 text-red-700"
                     }`}
                   >
                     {user.status}
                   </span>
-                </td>
-                <td className="border p-2 text-center">
-                  <button
-                    className="bg-indigo-200 hover:text-indigo-800 px-4 py-1 rounded shadow mx-2"
-                    onClick={() => handleEdit(user)}
-                  >
-                    {t("edit")}
-                  </button>
-                  <button
-                    className="bg-red-400 hover:text-red-800 px-4 py-1 rounded shadow mx-2"
-                    onClick={() => handleDeleteUser(user.id)}
-                  >
-                    {t("delete")}
-                  </button>
-                </td>
-              </tr>
-            ))}
-            {filteredUsers.length === 0 && (
-              <tr>
-                <td colSpan="6" className="text-center p-4 text-gray-500">
-                  No users found.
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
-      <div className="block md:hidden space-y-4">
-        {filteredUsers.map((user) => (
-          <div key={user.id} className="bg-white p-4 rounded shadow">
-            <div className="flex items-center gap-4 mb-2">
-              <img
-                src={user.profileImg}
-                alt={user.name}
-                className="w-16 h-16 rounded-full object-contain"
-              />
-              <div>
-                <h2 className="font-bold">{user.name}</h2>
-                <p className="text-sm text-gray-600">{user.email}</p>
+                </p>
+                <button
+                  className="mt-3 text-indigo-600 hover:underline"
+                  onClick={() => handleEdit(user)}
+                >
+                  <MdOutlineModeEdit />
+                </button>
+                <button
+                  className="mt-3 text-red-700 hover:underline"
+                  onClick={() => handleDeleteUser(user.id)}
+                >
+                  <RiDeleteBin6Line />
+                </button>
               </div>
-            </div>
-            <p>
-              <strong>Phone:</strong> {user.phoneNumber}
-            </p>
-            <p>
-              <strong>Status:</strong>
-              <span
-                className={`ml-1 px-2 py-1 rounded-full text-xs ${
-                  user.status === "active"
-                    ? "bg-green-100 text-green-700"
-                    : "bg-red-100 text-red-700"
-                }`}
-              >
-                {user.status}
-              </span>
-            </p>
-            <button
-              className="mt-3 text-indigo-600 hover:underline"
-              onClick={() => handleEdit(user)}
-            >
-              {t("edit")}
-            </button>
-            <button
-              className="mt-3 text-indigo-600 hover:underline"
-              onClick={handleDeleteUser}
-            >
-              DELETE
-            </button>
+            ))}
           </div>
-        ))}
-      </div></>}
-
+        </>
+      )}
 
       {/* Modal */}
       {isModalOpen && selectedUser && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50" >
-          <div className="bg-white p-6 rounded-lg w-[600px] max-h-[90vh] overflow-y-auto space-y-4 shadow-lg" >
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white p-6 rounded-lg w-[600px] max-h-[90vh] overflow-y-auto space-y-4 shadow-lg">
             <button
               className="  text-primary-light hover:text-gray-700 text-3xl"
               onClick={() => setIsModalOpen(false)}
             >
               <LuCircleX />
-
             </button>
             <h2 className="text-xl font-bold text-center mb-4">Edit User</h2>
-            
+
             <img
               src={selectedUser.profileImg}
               alt="Profile"
@@ -282,6 +322,21 @@ const UserList = () => {
                 />
               </div>
             ))}
+            <label className="block font-medium mb ">
+              {selectedUser.name} Meetings
+            </label>
+            <select
+              className="w-full border border-gray-300 p-2 rounded-md"
+              name="meetingId"
+            >
+              {selectedUser.meetings
+                .filter((meeting) => meeting.attended === true)
+                .map((meeting) => (
+                  <option key={meeting.id} value={meeting.id}>
+                    {meeting.title}
+                  </option>
+                ))}
+            </select>
             <label className="block font-medium mb-1">Status</label>
             <select
               name="status"
@@ -306,9 +361,9 @@ const UserList = () => {
             <div className="flex justify-end gap-4 pt-4">
               <button
                 className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
-                onClick={handleDeleteUser}
+                onClick={() => handleDeleteUser(selectedUser.id)}
               >
-                Delete
+                {t("Delete User")}
               </button>
               <button
                 className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
