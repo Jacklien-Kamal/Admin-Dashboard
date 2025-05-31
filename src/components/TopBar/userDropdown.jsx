99165e93cd0f297d0e729a133e1dc113daaa5c94
jacklien <159618517+Jacklien-Kamal@users.noreply.github.com>
2025-06-01 00:13:25 +0300

    user dropdown ui

--- src/components/TopBar/userDropdown.jsx
+++ src/components/TopBar/userDropdown.jsx
@@ -1,28 +1,53 @@
-import React, { useState } from 'react'
-import { FaUserCircle } from 'react-icons/fa';
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { FaUserCircle } from "react-icons/fa";
+import { LuDoorClosed, LuDoorOpen, LuSettings } from "react-icons/lu";
 
 export default function UserDropdown() {
-      const [menuOpen, setMenuOpen] = useState(false);
-    
+  const [menuOpen, setMenuOpen] = useState(false);
+  const { t } = useTranslation();
+
   return (
     <div>
-
-   <button onClick={() => setMenuOpen(!menuOpen)} onBlur={()=>{setMenuOpen(!menuOpen)}}>
-            <FaUserCircle className='' />
-          </button>  
-          
-          {/* User Menu */}
+      <button
+        onClick={() => setMenuOpen(!menuOpen)}
+        onBlur={() => {
+          setMenuOpen(!menuOpen);
+        }}
+      >
+        {/* User Profile */}
+        <div className="flex items-center gap-2">
+          <img
+            src="https://randomuser.me/api/portraits/men/32.jpg"
+            alt="avatar"
+            className="w-9 h-9 rounded-full object-cover"
+          />
+          <div className="text-left hidden sm:block">
+            <div className="text-sm font-semibold text-slate-700">
+              Json Taylor
+            </div>
+            <div className="text-xs text-gray-500">{t("Web Designer")}</div>
+          </div>
+        </div>
+      </button>
+      {/* User Menu */}
       {menuOpen && (
-          <div className="absolute text-sm mt-3 bg-primary-light text-white dark:bg-secondary-body  dark:text-black shadow rounded p-2 z-50 " onBlur={()=>setMenuOpen(!menuOpen)}>
-          <button className="block w-full text-left px-2 py-2 hover:bg-gray-700 hover:text-gray-100">
-            Profile
+        <div
+          className="absolute text-sm bg-white text-black dark:bg-primary-body  dark:text-white shadow rounded py-1 z-50 w-32 "
+          onBlur={() => setMenuOpen(!menuOpen)}
+        >
+          <button className="flex items-center gap-2 w-full text-left px-2 py-2 hover:bg-primary-light  hover:bg-opacity-80">
+            <FaUserCircle /> Profile
           </button>
-          <button className="block w-full text-left px-2 py-2 hover:bg-gray-700 hover:text-gray-100">
-            My Account
+
+          <button className="flex items-center gap-2 w-full text-left px-2 py-2 hover:bg-primary-light  hover:bg-opacity-80">
+            <LuSettings /> settings
+          </button>
+          <button className="flex items-center gap-2 w-full text-left px-2 py-2 hover:bg-primary-light  hover:bg-opacity-80">
+            <LuDoorOpen /> Logout{" "}
           </button>
         </div>
-      )}  
-      </div>
-        
-        )
+      )}
+    </div>
+  );
 }
