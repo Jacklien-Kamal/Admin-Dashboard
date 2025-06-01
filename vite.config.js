5458de59844438a13d7e7ec3aa56b556ab60a546
jacklien <159618517+Jacklien-Kamal@users.noreply.github.com>
2025-06-01 10:56:41 +0300

    some changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  root: './src', // if your main entry files are inside `src`
   plugins: [react()],
-  build: {
-    outDir: '../dist', // relative to root, where the build goes
-  },
-});
+})
